2e095e8120878a38492b6d433a97932de501f7a8
Dmitriy Shatalov <deman4ik@gmail.com>
2017-03-11 22:13:57 +0300

    Updated React-Native to 0.42

--- android/app/src/main/java/com/inasleepmobile/MainApplication.java
+++ android/app/src/main/java/com/inasleepmobile/MainApplication.java
@@ -1,13 +1,11 @@
 package com.inasleepmobile;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -20,7 +18,7 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    protected boolean getUseDeveloperSupport() {
+    public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
 
