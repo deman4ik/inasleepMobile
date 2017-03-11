34cd77bae365652e13ac455fed249f0b100a9daa
Dmitriy Shatalov <deman4ik@gmail.com>
2017-03-11 20:29:59 +0300

    Material Design Bottom Navigation

--- app/navigation/navigator.js
+++ app/navigation/navigator.js
@@ -1,6 +1,9 @@
+import React from 'react';
 import {
   StackNavigator, TabNavigator
 } from 'react-navigation';
+import { NavigationComponent } from 'react-native-material-bottom-navigation'
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import LoginPage from '../pages/LoginPage'
 import RegisterPage from '../pages/RegisterPage'
 import FlowPage from '../pages/FlowPage'
@@ -9,7 +12,7 @@ import SearchPage from '../pages/SearchPage'
 import NotificationsPage from '../pages/NotificationsPage'
 import ProfilePage from '../pages/ProfilePage'
 import SettingsPage from '../pages/SettingsPage'
-
+import colors from '../styles/colors'
 
 const AuthNavigator = StackNavigator({
   Login: { screen: LoginPage },
@@ -39,15 +42,40 @@ const MainNavigator = TabNavigator(
   },
   {
     headerMode: 'screen',
-  //...other configs
+    tabBarComponent: NavigationComponent,
+   tabBarPosition: 'bottom',
 tabBarOptions:{
-    // tint color is passed to text and icons (if enabled) on the tab bar
-    activeTintColor: 'white',
-    inactiveTintColor: 'blue',
-// background color is for the tab component
-    activeBackgroundColor: 'blue',
-    inactiveBackgroundColor: 'white',
+  bottomNavigationOptions: {
+      labelColor: 'white',
+      rippleColor: 'white',
+      tabs: {
+        Flow: {
+           label: 'Home',
+           icon: <Icon size={24} color={colors.blueGrayLight} name="home" />,
+           activeIcon: <Icon size={24} color="white" name="home" />,
+           barBackgroundColor: colors.gradientColorOne
+        },
+        Search: {
+           label: 'Search',
+           icon: <Icon size={24} color={colors.blueGrayLight}  name="search" />,
+           activeIcon: <Icon size={24} color="white" name="search" />,
+           barBackgroundColor: colors.gradientColorTwo
+        },
+        Notifications: {
+           label: 'Notifications',
+           icon: <Icon size={24} color={colors.blueGrayLight} name="notifications" />,
+           activeIcon: <Icon size={24} color="white" name="notifications" />,
+           barBackgroundColor: colors.gradientColorOne
+        },
+        Profile: {
+           label: 'Profile',
+           icon: <Icon size={24} color={colors.blueGrayLight} name="person" />,
+           activeIcon: <Icon size={24} color="white" name="person" />,
+           barBackgroundColor: colors.gradientColorTwo
+        }
+      }
   }
+}
 }
 );
 
--- app/pages/FlowPage/FlowPage.js
+++ app/pages/FlowPage/FlowPage.js
@@ -6,13 +6,14 @@ import {
   Button,
 } from 'react-native'
 import FlatButton from '../../components/FlatButton'
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default class FlowPage extends Component {
   render() {
       const { navToDreamPage } = this.props;
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>
+        <Icon size={24} color="white" name="home" /><Text>
           Flow Screen
         </Text>
   <FlatButton onPress={navToDreamPage} title='Go To Dream Page'/>
