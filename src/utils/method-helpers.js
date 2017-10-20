8deaf94f28a7771b7ce9229f4cee7bc39eae705d
Dmitriy Shatalov <deman4ik@gmail.com>
2017-10-20 19:02:32 +0300

    moment -> date-fns

--- src/utils/method-helpers.js
+++ src/utils/method-helpers.js
@@ -1,15 +1,14 @@
 import { NavigationActions } from "react-navigation";
-import moment from "moment/min/moment-with-locales.min";
 import I18n from "../locale";
 
 export const resetNavigationTo = (routeName: string, navigation: {}) => {
-	const resetAction = NavigationActions.reset({
-		index: 0,
-		key: null,
-		actions: [NavigationActions.navigate({ routeName })]
-	});
+    const resetAction = NavigationActions.reset({
+        index: 0,
+        key: null,
+        actions: [NavigationActions.navigate({ routeName })]
+    });
 
-	navigation.dispatch(resetAction);
+    navigation.dispatch(resetAction);
 };
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
@@ -19,10 +18,10 @@ export const delay = (delayed, ms) => Promise.all([delayed, sleep(ms)]).then(([d
 export const translate = (key, lang, interpolation = null) => I18n.t(key, { locale: lang, ...interpolation });
 
 export const configureLocale = language => {
-	I18n.locale = language;
-	I18n.fallbacks = true;
+    I18n.locale = language;
+    I18n.fallbacks = true;
 
-	moment.updateLocale(language, {
+    /*moment.updateLocale(language, {
 		relativeTime: translate("common.relativeTime", language)
-	});
+	});*/
 };
