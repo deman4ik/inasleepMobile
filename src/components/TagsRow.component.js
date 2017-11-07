f0e4824c1b43f12a3152f19308fdb23fbd1c6a49
Dmitriy Shatalov <deman4ik@gmail.com>
2017-11-07 19:13:28 +0300

    TagsRow with no content text

--- src/components/TagsRow.component.js
+++ src/components/TagsRow.component.js
@@ -5,78 +5,79 @@ import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 import { colors, normalize } from "../config";
 import { textStyles } from "../styles";
 const styles = StyleSheet.create({
-	container: {
-		flexGrow: 1,
-		flexShrink: 0,
-		flexDirection: "row",
-		justifyContent: "flex-start",
-		alignItems: "center",
-		backgroundColor: colors.white,
-		paddingHorizontal: 15,
-		height: 65
-	},
-	icon: {
-		color: "#222222",
-		textAlign: "center",
-		textAlignVertical: "center",
-		marginRight: 15
-	},
-	rightIcon: {
-		color: "#222222",
-		textAlign: "center",
-		textAlignVertical: "center",
-		opacity: 0.5
-	},
-	tagTextContainer: {
-		flex: 1,
-		flexDirection: "row"
-	},
-	tagText: {
-		paddingVertical: 5,
-		marginRight: 10,
-		backgroundColor: "#F2F2F2",
-		color: "black",
-		paddingHorizontal: 5
-	},
-	text: {
-		color: "#666666",
-		flex: 1
-	}
+    container: {
+        flexGrow: 1,
+        flexShrink: 0,
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        backgroundColor: colors.white,
+        paddingHorizontal: 15,
+        height: 65
+    },
+    icon: {
+        color: "#222222",
+        textAlign: "center",
+        textAlignVertical: "center",
+        marginRight: 15
+    },
+    rightIcon: {
+        color: "#222222",
+        textAlign: "center",
+        textAlignVertical: "center",
+        opacity: 0.5
+    },
+    tagTextContainer: {
+        flex: 1,
+        flexDirection: "row"
+    },
+    tagText: {
+        paddingVertical: 5,
+        marginRight: 10,
+        backgroundColor: "#F2F2F2",
+        color: "black",
+        paddingHorizontal: 5
+    },
+    text: {
+        color: "#666666",
+        flex: 1
+    }
 });
 export const TagsRow = props => {
-	const { icon, iconRight, tags, onPress, disabled } = props;
-	const tagsList = tags.map(tag => (
-		<TouchableOpacity key={tag} onPress={onPress}>
-			<Text style={styles.tagText}>{tag}</Text>
-		</TouchableOpacity>
-	));
+    const { icon, iconRight, tags, noTagsText, onPress, disabled } = props;
+    const tagsList = tags.map(tag => (
+        <TouchableOpacity key={tag} onPress={onPress} disabled={disabled}>
+            <Text style={styles.tagText}>{tag}</Text>
+        </TouchableOpacity>
+    ));
 
-	return (
-		<View style={styles.container}>
-			{icon !== null && <MaterialCommunityIcons size={normalize(22)} name={icon} style={styles.icon} />}
-			{tags.length > 0 && (
-				<ScrollView style={styles.tagTextContainer} horizontal showsHorizontalScrollIndicator={false}>
-					{tagsList}
-				</ScrollView>
-			)}
-			{tags.length == 0 && <Text style={[textStyles.text, styles.text]}>Нет ключевых слов</Text>}
-			{iconRight !== null && <Ionicons size={normalize(20)} name={iconRight} style={styles.rightIcon} />}
-		</View>
-	);
+    return (
+        <View style={styles.container}>
+            {icon !== null && <MaterialCommunityIcons size={normalize(22)} name={icon} style={styles.icon} />}
+            {tags.length > 0 ? (
+                <ScrollView style={styles.tagTextContainer} horizontal showsHorizontalScrollIndicator={false}>
+                    {tagsList}
+                </ScrollView>
+            ) : (
+                <Text style={[textStyles.text, styles.text]}>{noTagsText}</Text>
+            )}
+            {iconRight !== null && <Ionicons size={normalize(20)} name={iconRight} style={styles.rightIcon} />}
+        </View>
+    );
 };
 
 TagsRow.defaultProps = {
-	icon: null,
-	iconRight: null,
-	tags: null,
-	disabled: false,
-	onPress: null
+    icon: null,
+    iconRight: null,
+    tags: null,
+    disabled: false,
+    onPress: null
 };
 
 TagsRow.propTypes = {
-	icon: PropTypes.string,
-	iconRight: PropTypes.string,
-	tags: PropTypes.array,
-	disabled: PropTypes.bool,
-	onPress: PropTypes.func
+    icon: PropTypes.string,
+    iconRight: PropTypes.string,
+    tags: PropTypes.array,
+    disabled: PropTypes.bool,
+    onPress: PropTypes.func
 };
--- src/pages/DreamPage/DreamPage.js
+++ src/pages/DreamPage/DreamPage.js
@@ -2,127 +2,135 @@
 import React, { Component } from "react";
 import { StyleSheet, Button, View } from "react-native";
 import {
-	ParallaxScroll,
-	ViewContainer,
-	DreamHeader,
-	ReadMore,
-	Divider,
-	IconTextRow,
-	TagsRow,
-	Comment,
-	ShowMoreContentRow
+    ParallaxScroll,
+    ViewContainer,
+    DreamHeader,
+    ReadMore,
+    Divider,
+    LineDivider,
+    IconTextRow,
+    TagsRow,
+    Comment,
+    ShowMoreContentRow
 } from "../../components";
 import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 import { translate } from "../../utils";
 import { colors } from "../../config";
 import { textStyles } from "../../styles";
 const styles = StyleSheet.create({
-	container: {
-		backgroundColor: colors.greyLight,
-		justifyContent: "flex-start"
-	},
-	textContainer: {
-		backgroundColor: colors.white
-	},
-	detailContainer: {
-		backgroundColor: colors.white
-	}
+    container: {
+        backgroundColor: colors.greyLight,
+        justifyContent: "flex-start"
+    },
+    textContainer: {
+        backgroundColor: colors.white
+    },
+    detailContainer: {
+        backgroundColor: colors.white
+    }
 });
 export default class DreamPage extends Component {
-	render() {
-		const { dream } = this.props.navigation.state.params;
-		const { auth, navigation } = this.props;
-		const { language } = auth;
-		return (
-			<ViewContainer barColor={"light"}>
-				<ParallaxScroll
-					renderContent={() => <DreamHeader dream={dream} />}
-					backgroundImage={dream.image}
-					stickyTitle={dream.title}
-					navigateBack
-					navigation={navigation}
-					showMenu
-				>
-					<View style={styles.container}>
-						<View style={styles.textContainer}>
-							<ReadMore numberOfLines={5} showHide={false}>
-								{dream.text}
-							</ReadMore>
-						</View>
-						<Divider textLeft="ДЕТАЛИ" />
+    render() {
+        const { dream } = this.props.navigation.state.params;
+        const { auth, navigation } = this.props;
+        const { language } = auth;
+        return (
+            <ViewContainer barColor={"light"}>
+                <ParallaxScroll
+                    renderContent={() => <DreamHeader dream={dream} />}
+                    backgroundImage={dream.image}
+                    stickyTitle={dream.title}
+                    navigateBack
+                    navigation={navigation}
+                    showMenu
+                >
+                    <View style={styles.container}>
+                        <View style={styles.textContainer}>
+                            <ReadMore numberOfLines={5} showHide={false}>
+                                {dream.text}
+                            </ReadMore>
+                        </View>
+                        <Divider textLeft="ДЕТАЛИ" />
 
-						<View style={styles.detailContainer}>
-							<TagsRow icon="puzzle" tags={["Лошадь", "Дерево", "Летать", "Кровать", "Облака"]} />
+                        <View style={styles.detailContainer}>
+                            <TagsRow
+                                icon="puzzle"
+                                tags={["Лошадь", "Дерево", "Летать", "Кровать", "Облака"]}
+                                noTagsText="Нет ключевых слов"
+                            />
+                            <LineDivider />
+                            <IconTextRow
+                                icon="ios-information-circle"
+                                text={`${dream.interpetsCount} интерпретации`}
+                                iconRight="ios-arrow-forward-outline"
+                                onPress={() => {
+                                    return null;
+                                }}
+                            />
+                            <LineDivider />
+                            <TagsRow icon="account-star" tags={[]} noTagsText="Никто не приснился" />
+                            <LineDivider />
+                            <IconTextRow icon="ios-people" text="Виден всем" disabled />
+                            <LineDivider />
+                            <IconTextRow icon="ios-globe-outline" text={dream.location} disabled />
+                        </View>
 
-							<IconTextRow
-								icon="ios-information-circle"
-								text={`${dream.interpetsCount} интерпретации`}
-								iconRight="ios-arrow-forward-outline"
-								onPress={() => {
-									return null;
-								}}
-							/>
+                        <Divider textLeft="КОММЕНТАРИИ" />
+                        <View style={styles.detailContainer}>
+                            <Comment
+                                avatar="https://shoutem.github.io/img/ui-toolkit/examples/image-11.png"
+                                author="Dustin"
+                                date="20 минут назад"
+                                text={
+                                    "Banjo tote bag bicycle rights, High Life sartorial cray craft beer whatever street art fap. Hashtag typewriter banh mi, squid keffiyeh High."
+                                }
+                                onAuthorPress={() => {
+                                    return null;
+                                }}
+                            />
+                            <LineDivider />
+                            <Comment
+                                avatar="https://shoutem.github.io/img/ui-toolkit/examples/image-12.png"
+                                author="Malone"
+                                date="30 минут назад"
+                                text={
+                                    "Banjo tote bag bicycle rights, High Life sartorial cray craft beer whatever street art fap. Hashtag typewriter banh mi, squid keffiyeh High."
+                                }
+                                onAuthorPress={() => {
+                                    return null;
+                                }}
+                            />
+                            <LineDivider />
+                            <Comment
+                                avatar="https://shoutem.github.io/img/ui-toolkit/examples/image-12.png"
+                                author="Malone"
+                                date="30 минут назад"
+                                text={"Тоже самое снилось, к чему бы это?"}
+                                onAuthorPress={() => {
+                                    return null;
+                                }}
+                            />
 
-							<IconTextRow icon="ios-person-add-outline" text="Никто не приснился" disabled />
+                            <ShowMoreContentRow
+                                text="Посмотреть все комментарии"
+                                count={7}
+                                onPress={() => {
+                                    return null;
+                                }}
+                            />
 
-							<IconTextRow icon="ios-people" text="Виден всем" disabled />
-
-							<IconTextRow icon="ios-globe-outline" text={dream.location} disabled />
-						</View>
-
-						<Divider textLeft="КОММЕНТАРИИ" />
-						<View style={styles.detailContainer}>
-							<Comment
-								avatar="https://shoutem.github.io/img/ui-toolkit/examples/image-11.png"
-								author="Dustin"
-								date="20 минут назад"
-								text={
-									"Banjo tote bag bicycle rights, High Life sartorial cray craft beer whatever street art fap. Hashtag typewriter banh mi, squid keffiyeh High."
-								}
-								onAuthorPress={() => {
-									return null;
-								}}
-							/>
-							<Comment
-								avatar="https://shoutem.github.io/img/ui-toolkit/examples/image-12.png"
-								author="Malone"
-								date="30 минут назад"
-								text={
-									"Banjo tote bag bicycle rights, High Life sartorial cray craft beer whatever street art fap. Hashtag typewriter banh mi, squid keffiyeh High."
-								}
-								onAuthorPress={() => {
-									return null;
-								}}
-							/>
-							<Comment
-								avatar="https://shoutem.github.io/img/ui-toolkit/examples/image-12.png"
-								author="Malone"
-								date="30 минут назад"
-								text={"Тоже самое снилось, к чему бы это?"}
-								onAuthorPress={() => {
-									return null;
-								}}
-							/>
-
-							<ShowMoreContentRow
-								text="Посмотреть все комментарии"
-								count={7}
-								onPress={() => {
-									return null;
-								}}
-							/>
-							<IconTextRow
-								size={22}
-								icon="ios-chatboxes"
-								text="Добавить комментарий"
-								onPress={() => {
-									return null;
-								}}
-							/>
-						</View>
-					</View>
-				</ParallaxScroll>
-			</ViewContainer>
-		);
-	}
+                            <IconTextRow
+                                size={22}
+                                icon="ios-chatboxes"
+                                text="Добавить комментарий"
+                                onPress={() => {
+                                    return null;
+                                }}
+                            />
+                        </View>
+                    </View>
+                </ParallaxScroll>
+            </ViewContainer>
+        );
+    }
 }
